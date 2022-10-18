44e6d1aa1b85e3694686bbb341c9386e01e2ed82
Jan C. Rubio <janrubio@utexas.edu>
2022-10-18 01:20:46 -0500

    Developed the frontend for the project library (Check-in/out implementation)

--- App.js
+++ App.js
@@ -38,24 +38,29 @@ class ProjectData extends React.Component {
       currProjectUser: "",
       project_list: []
     };
-    this.state.project_list.push([0, "Project 0", "User 1", "HWSet 1: 50/100", "HWSet 2: 30/100"])
-    this.state.project_list.push([1, "Project 1", "User 2", "HWSet 1: 50/100", "HWSet 2: 0/100"])
-    this.state.project_list.push([2, "Project 2", "User 3", "HWSet 1: 50/50", "HWSet 2: 30/40"])
-    this.state.project_list.push([3, "Project 3", "User 4", "HWSet 1: 50/70", "HWSet 2: 30/50"])
+    this.state.project_list.push([0, "Project 0", "User 1", 0, [50, 0, 0], 100, [30, 0, 0], 100])
+    this.state.project_list.push([1, "Project 1", "User 2", 0, [50, 0, 0], 100, [0, 0, 0], 100])
+    this.state.project_list.push([2, "Project 2", "User 3", 0, [50, 0, 0], 50, [30, 0, 0], 50])
+    this.state.project_list.push([3, "Project 3", "User 4", 0, [50, 0, 0], 70, [30, 0, 0], 50])
   }
 
   // Create a single project with given data
-  renderProject(i, proj, usr, hw1, hw2) {
+  renderProject(i, proj, usr, hw1_avail, hw1_cap, hw2_avail, hw2_cap) {
     return(
       <Project
         key={i.toString()}  // "key" is recommended by console (don't use it much in project tho)
         idx = {i}
         Name = {proj}
         User = {usr}
-        HWSet1 = {hw1}
-        HWSet2 = {hw2}
+        HWSet1_Availability = {hw1_avail[0]}
+        HWSet1_Capacity = {hw1_cap}
+        HWSet2_Availability = {hw2_avail[0]}
+        HWSet2_Capacity = {hw2_cap}
         onCheckInClick={() => this.handleCheckIn(i)}
+        onCheckInValue={() => this.handleCheckInValue(i)}
         onCheckOutClick={() => this.handleCheckOut(i)}
+        onCheckOutValue={() => this.handleCheckOutValue(i)}
+        onHWSelection={() => this.handleHWSelection(i)}
       />
     )
   }
@@ -73,15 +78,17 @@ class ProjectData extends React.Component {
 
   // Update page with the data stored
   render() {
-    const new_project_list = []
-    const project_list = this.state.project_list.slice()
+    const new_project_list = [];
+    const project_list = this.state.project_list.slice();
     for(let i = 0; i < project_list.length; i++) {
       const project_data = project_list[i];
       new_project_list.push(this.renderProject(project_data[0],
                                                project_data[1],
                                                project_data[2],
-                                               project_data[3],
-                                               project_data[4]))
+                                               project_data[4],
+                                               project_data[5],
+                                               project_data[6],
+                                               project_data[7]));
     }
     return(
       <div>
@@ -94,22 +101,67 @@ class ProjectData extends React.Component {
 
   //// Handlers
 
+  handleHWSelection(i) {
+    const project_list = this.state.project_list.slice();
+    var currHWSelection = parseInt(document.getElementById("hwset").value);
+    project_list[i][3] = currHWSelection;
+    this.setState({
+      project_list: project_list
+    })
+    this.handleCheckInValue(i);
+    this.handleCheckOutValue(i);
+  }
+
   // Check-In HWSet
   handleCheckIn(i) {
     const project_list = this.state.project_list.slice();
-    project_list[i][3] = "HWSet 1: 100/100";
-    this.setState({
-      project_list: project_list
-    });
+    const hwIdx = project_list[i][3];
+    if(project_list[i][4 + 2*hwIdx][0] + project_list[i][4 + 2*hwIdx][1] <= project_list[i][4 + 2*hwIdx + 1]) {
+      project_list[i][4 + 2*hwIdx][0] += project_list[i][4 + 2*hwIdx][1];
+      project_list[i][4 + 2*hwIdx][1] = 0;
+      this.setState({
+        project_list: project_list
+      });
+      document.getElementById("checkIn").value = "";
+    }
+  }
+
+  handleCheckInValue(i) {
+    var newCheckInValue = parseInt(document.getElementById("checkIn").value);
+    if(!isNaN(newCheckInValue)) {
+      const project_list = this.state.project_list.slice();
+      const hwIdx = project_list[i][3];
+      project_list[i][4 + 2*hwIdx][1] = newCheckInValue;
+      this.setState({
+        project_list: project_list
+      })
+    }
   }
 
   // Check-In HWSet
   handleCheckOut(i) {
     const project_list = this.state.project_list.slice();
-    project_list[i][3] = "HWSet 1: 0/100";
-    this.setState({
-      project_list: project_list
-    });
+    const hwIdx = project_list[i][3];
+    if(project_list[i][4 + 2*hwIdx][0] - project_list[i][4 + 2*hwIdx][2] >= 0) {
+      project_list[i][4 + 2*hwIdx][0] -= project_list[i][4 + 2*hwIdx][2];
+      project_list[i][4 + 2*hwIdx][2] = 0;
+      this.setState({
+        project_list: project_list
+      });
+      document.getElementById("checkOut").value = "";
+    }
+  }
+
+  handleCheckOutValue(i) {
+    var newCheckOutValue = parseInt(document.getElementById("checkOut").value);
+    if(!isNaN(newCheckOutValue)) {
+      const project_list = this.state.project_list.slice();
+      const hwIdx = project_list[i][3];
+      project_list[i][4 + 2*hwIdx][2] = newCheckOutValue;
+      this.setState({
+        project_list: project_list
+      })
+    }
   }
 
   // Add new HWSet to Data
@@ -119,7 +171,7 @@ class ProjectData extends React.Component {
     if(typeof projectName === 'string' && typeof projectUser === 'string') {
       if(projectName.trim() !== '' && projectUser.trim() !== '') {
         const project_list = this.state.project_list.slice();
-        project_list.push([project_list.length, projectName, projectUser, "HWSet 1: 55/70", "HWSet 2: 31/50"]);
+        project_list.push([project_list.length, projectName, projectUser, 0, [50, 0, 0], 100, [30, 0, 0], 50]);
         this.setState({
           project_list: project_list,
         })
@@ -161,30 +213,58 @@ function Project(props) {
         </div>
         {/* Sets available */}
         <div className="column">
-          <p className="hwDescription">{props.HWSet1}</p>
-          <p className="hwDescription">{props.HWSet2}</p>
+          <p className="hwDescription">HWSet1: {props.HWSet1_Availability}/{props.HWSet1_Capacity}</p>
+          <p className="hwDescription">HWSet2: {props.HWSet2_Availability}/{props.HWSet2_Capacity}</p>
         </div>
         {/* Select HW */}
         <div className="column">
           <p className="hwDescription">Select HWSet:</p>
-          <select className="hwSelect" name="hwset" id="hwset">
-            <option value="hws1">HWSet 1</option>
-            <option value="hws1">HWSet 2</option>
+          <select className="hwSelect"
+                  id="hwset"
+                  name="hwset"
+                  onChange={props.onHWSelection}
+          >
+            <option value="0">HWSet 1</option>
+            <option value="1">HWSet 2</option>
           </select>
         </div>
-        {/* Check In */}
+        {/* Check In */} 
         <div className="column">
-          <input className="hwInput" type="text" placeholder="Enter Value"/>
-          <button className="checkBtn" type="button" onClick={props.onCheckInClick}>Check In</button>
+          <input className="hwInput"
+                 id="checkIn"
+                 type="text"
+                 placeholder="Enter Value"
+                 onChange={props.onCheckInValue}
+          />
+          <button className="checkBtn"
+                  type="button"
+                  onClick={props.onCheckInClick}
+          >
+            Check In
+          </button>
         </div>
-        {/* Check Out*/}
+        {/* Check Out */}
         <div className="column">
-          <input className="hwInput" type="text" placeholder="Enter Value"/>
-          <button className="checkBtn" type="button" onClick={props.onCheckOutClick}>Check Out</button>
+          <input className="hwInput"
+                 id="checkOut"
+                 type="text"
+                 placeholder="Enter Value"
+                 onChange={props.onCheckOutValue}
+          />
+          <button className="checkBtn"
+                  type="button"
+                  onClick={props.onCheckOutClick}
+          >
+            Check Out
+          </button>
         </div>
         {/* Join or Leave */}
         <div className="column">
-          <button className="joinBtn" type="button">Join</button>
+          <button className="joinBtn"
+                  type="button"
+          >
+            Join
+          </button>
         </div>
     </div>
   )
