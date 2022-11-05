ac39ed95d22aebd2e1883fb95442612045499d3c
Jan C. Rubio <janrubio@utexas.edu>
2022-11-05 17:52:39 -0500

    \'app.py\' contains an example route \'test\' that adds some projects to the frontend

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
-from flask import Flask, send_from_directory, jsonify, request
+from flask import Flask, send_from_directory, jsonify
 
-app = Flask(__name__, static_url_path='', static_folder='build/')
+app = Flask(__name__, static_url_path='', static_folder='frontend/build/')
 
 @app.route('/')
 def index():
-    return send_from_directory('build/', 'index.html')
+    return send_from_directory('frontend/build/', 'index.html')
+
+""" Test out sending data to frontend. """
+@app.route('/test')
+def test():
+    return send_from_directory('backend/', 'test.json')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
