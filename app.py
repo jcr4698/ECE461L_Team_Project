84b5e174f513bfe7ddaf57226bef9ca2e0932731
Arsh Gamare <arsh.gamare@gmail.com>
2022-11-05 19:00:56 -0500

    Login API function, encryption, and decryption

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, send_from_directory, jsonify, request
 from backend.container import Project, HWSet, User
+from backend.project_database_access import *
+from encrypt import encrypt
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build/')
 
@@ -11,30 +13,55 @@ app = Flask(__name__, static_url_path='', static_folder='frontend/build/')
 def index():
     return send_from_directory('frontend/build/', 'index.html')
 
-""" Test out sending data to frontend. """
-@app.route('/test')
-def test():
-    return send_from_directory('backend/', 'test.json')
-
-# ----------------------------
+# ------------------------------------------------------------------------------
 # BEGIN USER LOGIN AND REGISTER HANDLING FUNCTIONS
-# ----------------------------
+# ------------------------------------------------------------------------------
 @app.route('/login', methods=['POST'])
 def login():
-    pass
+    # Get request
+    request = request.get_json()
+    username = request['username']
+    password = request['password']
+    
+    # Check if username from request is a username in the database 
+    # Return 404 NOT FOUND ERROR if username not in usernames
+    usernames = get_usernames()
+    if username not in usernames:
+        return {
+            'username' : 'User does not exist',
+            'password' : ''
+        }, 404
+
+    # Encrypt password and check if it is same as password in database
+    # Return 401 UNAUTHORIZED ERROR if password is not correct for username in database
+    encrypted_request_password = encrypt(password, 1, 6)
+    password_from_database = get_password_by_username(username)
+    if not (encrypted_request_password == password_from_database):
+        return {
+            'username' : username,
+            'password' : 'Password is not correct'
+        }, 401
+
+    # Valid login by this point so log them in by returning encrypted password as "access token" 
+    # Return 200 SUCCESSFUL code for successful login
+    return {
+        'password' : password_from_database
+    }, 200
+
+    
 
 @app.route('/register', methods=['POST'])
 def register():
     pass
 
-# ----------------------------
+# ------------------------------------------------------------------------------
 # END USER LOGIN AND REGISTER HANDLING FUNCTIONS
-# ----------------------------
+# ------------------------------------------------------------------------------
 
 
-# ----------------------------
+# ------------------------------------------------------------------------------
 # BEGIN PROJECT HANDLING FUNCTIONS
-# ----------------------------
+# ------------------------------------------------------------------------------
 @app.route('/', methods=['POST'])
 def create_projects():
     pass
@@ -51,14 +78,14 @@ def get_project_by_id(id):
 @app.route('/', methods=['POST'])
 def update_project_by_id(id):
     pass
-# ----------------------------
+# ------------------------------------------------------------------------------
 # END PROJECT HANDLING FUNCTION
-# ----------------------------
+# ------------------------------------------------------------------------------
 
 
-# ----------------------------
+# ------------------------------------------------------------------------------
 # BEGIN HWSET HANDLING FUNCTIONS
-# ----------------------------
+# ------------------------------------------------------------------------------
 @app.route('/', methods=['POST'])
 def create_hardware():
     pass
@@ -75,12 +102,9 @@ def get_hardware_by_id(id):
 def update_hardware_by_id(id):
     pass
 
-# ----------------------------
+# ------------------------------------------------------------------------------
 # END HWSET HANDLING FUNCTIONS
-# ----------------------------
-
-
-
+# ------------------------------------------------------------------------------
 
 
 
