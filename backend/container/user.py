a461289e49f4221e9648d958c2c7212d82ebd68c
sydthompson <sydneythompson@utexas.edu>
2022-11-08 15:48:56 -0600

    User class

---
+++ backend/container/user.py
@@ -0,0 +1,30 @@
+
+
+class User():
+
+    def __init__(self, userId, username, password, project_list):
+        self.userId=userId
+        self.username=username
+        self.password=password
+        self.project_list=project_list
+
+
+    def set_userId(self, userId):
+        self.userId=userId
+    def get_userId(self):
+        return self.userId
+
+    def set_username(self, username):
+        self.username=username
+    def get_username(self):
+        return self.username
+
+    def set_password(self, password):
+        self.password=password
+    def get_password(self):
+        return self.password
+
+    def set_project_list(self, project_list):
+        self.project_list=project_list
+    def get_project_list(self):
+        return self.project_list
