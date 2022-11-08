07ffdf9cf018f53d3bf52e951b1a7b3c2accd9fa
Jan C. Rubio <janrubio@utexas.edu>
2022-11-08 15:59:04 -0600

    connecting api with react 1.0

---
+++ backend/container/project.py
@@ -0,0 +1,41 @@
+import hardware_set
+import json
+from json import JSONEncoder
+from types import SimpleNamespace
+
+class Project:
+
+    def __init__(self, id: str, hw_set1: hardware_set.HWSet, hw_set2: hardware_set.HWSet, auth_users: list):
+        self.id=id
+        self.hw_set1=hw_set1
+        self.hw_set2=hw_set2
+        self.auth_users=auth_users
+
+    def get_id(self):
+        return self.id
+
+    def get_hw_sets(self):
+        return self.hw_set1, self.hw_set2
+    
+    def get_auth_users(self):
+        return self.auth_users
+    
+    def set_id(self, id):
+        self.id=id
+
+    def set_hw_set1(self, hw1: hardware_set.HWSet):
+        self.hw_set1=hw1
+
+    def set_hw_set2(self, hw2: hardware_set.HWSet):
+        self.hw_set2=hw2
+    
+    def set_auth_users(self, users: list):
+        self.auth_users=users
+
+
+class ProjectEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+def project_to_json(project: Project):
+    return json.dumps(project, indent=4, cls=ProjectEncoder)
---
+++ backend/project_database_access.py
@@ -0,0 +1,144 @@
+from pymongo import MongoClient
+# import container.project
+# import container.hardware_set
+import json
+import math
+
+def open_connection():
+    client=MongoClient("mongodb+srv://user:1234@cluster0.gdxcywm.mongodb.net/?retryWrites=true&w=majority")
+    db=client['project_app']
+    table=db['Projects']
+    return client, table
+
+def close_connection(client: MongoClient):
+    client.close()
+
+def get_all_projects():
+    client, table=open_connection()
+    close_connection(client)
+    return table.find()
+
+def checkout_hw(id: str, hw_set: int, num: int, user: str) -> bool:
+    client, table=open_connection()
+    proj= table.find({"id":id})
+
+    if len(proj) == 0:
+        close_connection(client)
+        return False
+
+    x=proj[0]
+
+    auth_users= json.loads(x.auth_users)
+    if user not in auth_users:
+        close_connection(client)
+        return False
+
+    if hw_set == '1':
+        availability=x.hw_set1.availability
+        newVal= {"$set":{"hw_set1.availability":math.max(0, availability-num)}}
+    else:
+        availability=x.hw_set2.availability
+        newVal= {"$set":{"hw_set2.availability":math.max(0, availability-num)}}
+
+
+    table.update_one({"id":id}, newVal)
+
+    close_connection(client)
+    return True
+
+
+
+def checkin_hw(id: str, hw_set: int, num: int, user: str) -> bool:
+    client, table=open_connection()
+    proj= table.find({"id":id})
+
+    if len(proj) == 0:
+        close_connection(client)
+        return False
+
+    x=proj[0]
+
+    auth_users= json.loads(x.auth_users)
+    if user not in auth_users:
+        close_connection(client)
+        return False
+
+    if hw_set == '1':
+        capacity=x.hw_set1.capacity
+        availability=x.hw_set1.availabilty
+        newVal= {"$set":{"hw_set1.availability":math.min(capacity, availability+num)}}
+
+    else:
+        capacity=x.hw_set2.capacity
+        availability=x.hw_set2.availabilty
+        newVal= {"$set":{"hw_set2.availability":math.min(capacity, availability+num)}}
+
+    table.update_one({"id":id}, newVal)
+
+    close_connection(client)
+    return True
+
+
+
+def join_project(id: str, user: str):
+    client, table=open_connection()
+
+    proj= table.find({"id":id})
+
+    if len(proj) == 0:
+        close_connection(client)
+        return False
+
+    x=proj[0]
+
+    auth_users= json.loads(x.auth_users)
+    if user not in auth_users:
+        auth_users.append(user)
+        table.update_one({"id":id}, {"$set": {"auth_users":auth_users}})    
+
+    close_connection(client)
+
+
+
+def leave_project(id: str, user: str):
+    client, table=open_connection()
+
+    proj= table.find({"id":id})
+
+    if len(proj) == 0:
+        close_connection(client)
+        return False
+
+    x=proj[0]
+
+    auth_users= json.loads(x.auth_users)
+    if user in auth_users:
+        auth_users.remove(user)
+        table.update_one({"id":id}, {"$set": {"auth_users":auth_users}})    
+
+    close_connection(client)
+
+
+
+def add_project(id:str, hw_set_1_qty: int, hw_set_2_qty: int, user_list: list)-> bool:
+    client, table=open_connection()
+
+    query = {"id":id}
+    results=table.find(query)
+
+    if len(results) != 0:
+        close_connection(client)
+        return False
+
+    
+    hw_1= container.hardware_set.HWSet(hw_set_1_qty, user_list)
+    hw_2= container.hardware_set.HWSet(hw_set_2_qty, user_list)
+
+    proj_def= container.project.Project(id, hw_1, hw_2, user_list)
+    proj_json= container.project.project_to_json(proj_def)    
+
+    table.insert_one(proj_json)
+
+    close_connection(client)
+
+    return True
