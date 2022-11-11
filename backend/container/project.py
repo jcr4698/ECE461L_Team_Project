bef97e3afad362d59461142559d51c965170c64a
sydney <sydneythompson@utexas.edu>
2022-11-11 15:25:02 -0600

    Updating projects

--- backend/container/project.py
+++ backend/container/project.py
@@ -5,19 +5,14 @@ from types import SimpleNamespace
 
 class Project:
 
-    def __init__(self, id: str, name, description, hw_set1: hardware_set.HWSet, hw_set2: hardware_set.HWSet, auth_users: list):
+    def __init__(self, id: str, name, description, auth_users: list):
         self.id = id
         self.name = name
         self.description = description
-        self.hw_set1 = hw_set1
-        self.hw_set2 = hw_set2
         self.auth_users = auth_users
 
     def get_id(self):
         return self.id
-
-    def get_hw_sets(self):
-        return self.hw_set1, self.hw_set2
     
     def get_auth_users(self):
         return self.auth_users
@@ -25,12 +20,6 @@ class Project:
     def set_id(self, id):
         self.id=id
 
-    def set_hw_set1(self, hw1: hardware_set.HWSet):
-        self.hw_set1=hw1
-
-    def set_hw_set2(self, hw2: hardware_set.HWSet):
-        self.hw_set2=hw2
-    
     def set_auth_users(self, users: list):
         self.auth_users=users
 
